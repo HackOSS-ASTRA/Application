8f1ab00a4ae2845e3ca074eeb0a7f24012e043f2
jaredpek <83465527+jaredpek@users.noreply.github.com>
2023-05-19 18:27:51 +0800

    [AP-17] Auto Obtain Daily Price Data (#8)
    * feature: Auto obtain daily stock prices
    
    * feature: Auto obtain daily stock prices
    
    * feature: Update auto obtain daily stock data
    
    * feature: Update auto obtain daily stock data

--- backend/project/asset/views.py
+++ backend/project/asset/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from asset.serializers import AssetSerializer, AssetHistorySerializer
 from asset.models import Asset, AssetHistory
 from rest_framework.permissions import AllowAny
+from portfolio.models import PortfolioItem
 
 class AssetDetailsView(generics.ListCreateAPIView):
     queryset = Asset.objects.all()
@@ -43,9 +44,33 @@ class UpdateAssetDetailsView(generics.UpdateAPIView):
     def put(self, request, *args, **kwargs):
         user = self.request.user
         if user.is_superuser or user.is_staff:
-            return super().put(request, *args, **kwargs)
+            data = self.request.data
+            asset_serializer = AssetSerializer(data=data)
+            if asset_serializer.is_valid(raise_exception=True):
+                asset = Asset.objects.get(id=self.kwargs['pk'])
+                asset.active = data.get('active')
+                asset.asset_name = data.get('asset_name')
+                asset.asset_type = data.get('asset_type')
+                asset.primary_exchange = data.get('primary_exchange')
+                asset.price_date = data.get('price_date')
+                asset.open_price = data.get('open_price')
+                asset.close_price = data.get('close_price')
+                asset.low_price = data.get('low_price')
+                asset.high_price = data.get('high_price')
+                asset.adjusted_close_price = data.get('adjusted_close_price')
+                asset.volume = data.get('volume')
+                asset.currency = data.get('currency')
+                asset.save()
+                port_items = PortfolioItem.objects.filter(asset=asset).all()
+                for port_item in port_items:
+                    old_value = port_item.current_value
+                    port_item.current_value = float(port_item.asset.close_price) * float(port_item.quantity)
+                    port_item.portfolio.net_worth += (port_item.current_value - old_value)
+                    port_item.save()
+                    port_item.portfolio.save()
+                return Response({'success': 'asset updated successfully'}, status.HTTP_200_OK)
+            return Response({'error': asset_serializer.errors}, status.HTTP_200_OK)
         return Response({'error': 'you are not authorised to edit stock data'}, status.HTTP_401_UNAUTHORIZED)
-
 class AssetHistoryView(generics.ListCreateAPIView):
     queryset = AssetHistory.objects.all()
     serializer_class = AssetHistorySerializer
