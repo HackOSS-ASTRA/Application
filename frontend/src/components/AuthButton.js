7976fc37c6fda3eab7fedbad2f437952846ca85b
tylergoh <tylergoh50@gmail.com>
2023-06-16 14:50:23 +0800

    feature: Implement Account Authentication

---
+++ frontend/src/components/AuthButton.js
@@ -0,0 +1,65 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Menu, MenuItem, Button } from "@mui/material";
+
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const AuthButton = () => {
+  const router = useRouter();
+  const [anchorElement, setAnchorElement] = useState(null);
+  const open = Boolean(anchorElement);
+  const handleClick = (event) => {
+    setAnchorElement(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorElement(null);
+  };
+
+  const { data: session, status } = useSession();
+  if (router.pathname == "/login") return <></>;
+  if (status == "authenticated") {
+    return (
+      <>
+        <Button style={{ color: "white" }} onClick={handleClick}>
+          {session.name}
+        </Button>
+        <Menu
+          anchorEl={anchorElement}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem
+            onClick={() => {
+              signOut();
+              setAnchorElement(null);
+            }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </>
+    );
+  } else {
+    return (
+      <Button
+        style={{ color: "white" }}
+        onClick={() => {
+          signIn();
+        }}
+      >
+        {" "}
+        Login
+      </Button>
+    );
+  }
+};
+
+export default AuthButton;
---
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,76 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "username", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        const payload = {
+          username: credentials.username,
+          password: credentials.password,
+        };
+
+        const url = process.env.NEXT_PUBLIC_SERVER_URL + "api/auth/login/";
+
+        const res = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        const responseData = await res.json();
+        let user = {};
+        user.accessToken = responseData.key;
+
+        if (res.ok && user) {
+          return user;
+        }
+        if (!res.ok) {
+          throw new Error(user.message);
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  callbacks: {
+    async signIn() {
+      return true;
+    },
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = user.accessToken;
+        const url =
+          process.env.NEXT_PUBLIC_SERVER_URL + "api/user_account/details/";
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Token ${token.accessToken}`,
+          },
+        });
+        const result = await response.json();
+        console.log(result);
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.name = token.name;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+});
