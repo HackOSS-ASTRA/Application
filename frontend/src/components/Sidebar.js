27231c8543c480f8fd766b7c71a7d2ce388692e0
tylergoh <tylergoh50@gmail.com>
2023-06-21 13:27:15 +0800

    feature: Implement Account Page

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -7,82 +7,81 @@ import {
   ListItemIcon,
   ListItemText,
   Typography,
-  Toolbar
+  Toolbar,
 } from "@mui/material";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import NewspaperIcon from "@mui/icons-material/Newspaper";
 import { BusinessCenter } from "@mui/icons-material";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 
 function Sidebar() {
   const router = useRouter();
-  const drawerWidth = 240;
+  const drawerWidth = 350;
   const highlight = new Array(2).fill({});
-  if(router.pathname == "/")
-    highlight[0] = {backgroundColor:"#1E1E1E"}
-  if(router.pathname == "/discover")
-    highlight[1] = {backgroundColor:"#1E1E1E"}
-  if(router.pathname == "/portfolio")
-    highlight[2] = {backgroundColor:"#1E1E1E"}
+  if (router.pathname == "/") highlight[0] = { backgroundColor: "#1E1E1E" };
+  if (router.pathname == "/discover")
+    highlight[1] = { backgroundColor: "#1E1E1E" };
+  if (router.pathname == "/portfolio")
+    highlight[2] = { backgroundColor: "#1E1E1E" };
   return (
     <div>
       <Toolbar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <List>
-          <ListItem>
-            <Avatar style={{ margin: "10px" }} alt="none" src="/logo.jpeg" />
-            <Typography
-              variant="h5"
-              sx={{
-                mr: 2,
-                display: "inline",
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: "rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              ASTRA
-            </Typography>
-          </ListItem>
-          <ListItem sx={highlight[0]}>
-            <ListItemButton href="/">
-              <ListItemIcon>
-                <BarChartIcon />
-              </ListItemIcon>
-              <ListItemText primary="Trading" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem sx={highlight[1]}>
-            <ListItemButton href="/discover">
-              <ListItemIcon>
-                <NewspaperIcon />
-              </ListItemIcon>
-              <ListItemText primary="Discover" />
-            </ListItemButton>
-          </ListItem >
-          <ListItem sx={highlight[2]}>
-            <ListItemButton href="/portfolio">
-              <ListItemIcon>
-                <BusinessCenter />
-              </ListItemIcon>
-              <ListItemText primary="Portfolio" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Drawer>
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <List>
+            <ListItem>
+              <Avatar style={{ margin: "10px" }} alt="none" src="/logo.jpeg" />
+              <Typography
+                variant="h5"
+                sx={{
+                  mr: 2,
+                  display: "inline",
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: "rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                ASTRA
+              </Typography>
+            </ListItem>
+            <ListItem sx={highlight[0]}>
+              <ListItemButton href="/">
+                <ListItemIcon>
+                  <BarChartIcon />
+                </ListItemIcon>
+                <ListItemText primary="Trading" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem sx={highlight[1]}>
+              <ListItemButton href="/discover">
+                <ListItemIcon>
+                  <NewspaperIcon />
+                </ListItemIcon>
+                <ListItemText primary="Discover" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem sx={highlight[2]}>
+              <ListItemButton href="/portfolio">
+                <ListItemIcon>
+                  <BusinessCenter />
+                </ListItemIcon>
+                <ListItemText primary="Portfolio" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
       </Toolbar>
     </div>
   );
