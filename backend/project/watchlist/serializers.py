0299f89cc56f31b5e910677bc68e10baf2220893
jaredpek <jpekjaredpek@gmail.com>
2023-07-02 00:49:40 +0800

    fix: Fix Watchlist and Portfolio API

--- backend/project/watchlist/serializers.py
+++ backend/project/watchlist/serializers.py
@@ -2,10 +2,39 @@ from rest_framework import serializers
 from watchlist.models import WatchlistItem
 
 class UserWatchlistSerializer(serializers.ModelSerializer):
+    asset_id = serializers.CharField(source='asset.asset_id', read_only=True)
     asset_name = serializers.CharField(source='asset.asset_name', read_only=True)
-    close_price = serializers.FloatField(source='asset.close_price', read_only=True)
-    currency = serializers.CharField(source='asset.currency', read_only=True)
+    asset_type = serializers.CharField(source='asset.asset_type', read_only=True)
     primary_exchange = serializers.CharField(source='asset.primary_exchange', read_only=True)
+    price_date = serializers.DateTimeField(source='asset.price_date', read_only=True)
+    open_price = serializers.FloatField(source='asset.open_price', read_only=True)
+    close_price = serializers.FloatField(source='asset.close_price', read_only=True)
+    low_price = serializers.FloatField(source='asset.low_price', read_only=True)
+    high_price = serializers.FloatField(source='asset.high_price', read_only=True)
+    adjusted_close_price = serializers.FloatField(source='asset.adjusted_close_price', read_only=True)
+    volume = serializers.FloatField(source='asset.volume', read_only=True)
+    currency = serializers.CharField(source='asset.currency', read_only=True)
+    change_proportion = serializers.FloatField(source='asset.change_proportion', read_only=True)
+    change_amount = serializers.FloatField(source='asset.change_amount', read_only=True)
+
     class Meta:
         model = WatchlistItem
-        fields = ['id', 'user', 'asset', 'asset_name', 'close_price', 'currency', 'primary_exchange']
+        fields = [
+            'id', 
+            'user', 
+            'asset', 
+            'asset_id',
+            'asset_name', 
+            'asset_type', 
+            'primary_exchange', 
+            'price_date',
+            'open_price', 
+            'close_price', 
+            'low_price', 
+            'high_price',
+            'adjusted_close_price',
+            'volume',
+            'currency',
+            'change_proportion',
+            'change_amount',
+        ]
--- backend/project/watchlist/views.py
+++ backend/project/watchlist/views.py
@@ -11,24 +11,21 @@ class UserWatchlistView(generics.ListCreateAPIView):
         self.queryset = self.queryset.filter(user=user_id)
         return self.queryset
     def post(self, request, *args, **kwargs):
-        data = self.request.POST
+        data = request.data
         user_id = self.request.user.id
         asset_id = data.get('asset')
         if WatchlistItem.objects.filter(user=user_id, asset=asset_id):
             return Response({'error': 'asset already exists in your watchlist'}, status.HTTP_406_NOT_ACCEPTABLE)
         else:
-            data._mutable = True
             data['user'] = user_id
-            data._mutable = False
             return super().post(request, *args, **kwargs)
 
 class DeleteWatchlistItemView(generics.DestroyAPIView):
     queryset = WatchlistItem.objects.all()
     def destroy(self, request, *args, **kwargs):
         try:
-            item = WatchlistItem.objects.get(id=self.kwargs['pk'])
-            if item.user.id == self.request.user.id:
-                return super().destroy(request, *args, **kwargs)
-            raise Exception
+            item = WatchlistItem.objects.get(user=self.request.user, asset=self.kwargs['pk'])
+            item.delete()
+            return Response({'success': 'asset removed from watchlist'})
         except:
-            return Response({'error': 'watchlist item not found'}, status.HTTP_406_NOT_ACCEPTABLE)
+            return Response({'error': 'asset not added to watchlist'}, status.HTTP_400_BAD_REQUEST)
