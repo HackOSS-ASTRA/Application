6c84f0b5c67b66faa58d39bd26a87234b3fd7928
jaredpek <83465527+jaredpek@users.noreply.github.com>
2023-07-03 10:30:28 +0800

    [AP-23] Fix Watchlist and Portfolio API (#20)
    * fix: Fix Watchlist and Portfolio API
    
    * fix: Fix Watchlist API

--- backend/project/asset/serializers.py
+++ backend/project/asset/serializers.py
@@ -2,9 +2,28 @@ from rest_framework import serializers
 from asset.models import Asset, AssetHistory
 
 class AssetSerializer(serializers.ModelSerializer):
+    asset = serializers.CharField(source='id', read_only=True)
     class Meta:
         model = Asset
-        fields = '__all__'
+        fields = [
+            'id',
+            'active',
+            'asset',
+            'asset_id',
+            'asset_name',
+            'asset_type',
+            'primary_exchange',
+            'price_date',
+            'open_price',
+            'close_price',
+            'low_price',
+            'high_price',
+            'adjusted_close_price',
+            'volume',
+            'currency',
+            'change_proportion',
+            'change_amount',
+        ]
 
 class AssetHistorySerializer(serializers.ModelSerializer):
     class Meta:
