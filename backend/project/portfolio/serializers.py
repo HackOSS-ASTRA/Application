a2ed006456326048a84302f20ea542ac2ca76843
jaredpek <jpekjaredpek@gmail.com>
2023-07-07 13:00:49 +0800

    fix: Fix Watchlist and Portfolio API

--- backend/project/portfolio/serializers.py
+++ backend/project/portfolio/serializers.py
@@ -47,10 +47,39 @@ class PortfolioItemSerializer(serializers.ModelSerializer):
     asset_type = serializers.CharField(source='asset.asset_type', read_only=True)
     primary_exchange = serializers.CharField(source='asset.primary_exchange', read_only=True)
     currency = serializers.CharField(source='asset.currency', read_only=True)
+    price_date = serializers.DateTimeField(source='asset.price_date', read_only=True)
+    open_price = serializers.FloatField(source='asset.open_price', read_only=True)
+    close_price = serializers.FloatField(source='asset.close_price', read_only=True)
+    low_price = serializers.FloatField(source='asset.low_price', read_only=True)
+    high_price = serializers.FloatField(source='asset.high_price', read_only=True)
+    adjusted_close_price = serializers.FloatField(source='asset.adjusted_close_price', read_only=True)
+    volume = serializers.FloatField(source='asset.volume', read_only=True)
+    change_proportion = serializers.FloatField(source='asset.change_proportion', read_only=True)
+    change_amount = serializers.FloatField(source='asset.change_amount', read_only=True)
 
     class Meta:
         model = PortfolioItem
-        fields = ['asset', 'asset_id', 'asset_name', 'asset_type', 'primary_exchange', 'currency', 'quantity', 'total_cost', 'average_cost', 'current_value']
+        fields = [
+            'asset',
+            'asset_id',
+            'asset_name',
+            'asset_type',
+            'primary_exchange',
+            'currency',
+            'quantity',
+            'total_cost',
+            'average_cost',
+            'current_value',
+            'price_date',
+            'open_price',
+            'close_price',
+            'low_price',
+            'high_price',
+            'adjusted_close_price',
+            'volume',
+            'change_proportion',
+            'change_amount',
+        ]
 
 class TransactionItemSerializer(serializers.ModelSerializer):
     asset_id = serializers.CharField(source='asset.asset_id', read_only=True)
